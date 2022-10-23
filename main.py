3732f5b14f69fb9b7427c9ec49e21d05b15e9f2b
Alex <mwppl123@gmail.com>
2022-10-23 11:13:47 +0200

    mine creation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 pygame.init()
 
 WIDTH, HEIGHT = 500, 600
@@ -6,6 +7,22 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("MineSweeper")
 
 BG_COLOR = "white"
+ROWS, COLS = 30,30
+MINES = 15
+def create_minefield(rows,cols, mines):
+    field =[[0 for _ in range(cols)] for _ in range(rows)]
+    mine_positions = set()
+
+    while len(mine_positions) < mines:
+        row = random.randrange(0,rows)
+        col = random.randrange(0,cols)
+        pos = row, col
+
+        if pos in mine_positions:
+            continue
+
+        mine_positions.add(pos)
+        field[row][col] = -1
 
 def draw(window):
     window.fill(BG_COLOR)
