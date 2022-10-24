54f71e1dcac5ef3b692462893b7ef29a05e47688
Alex <mwppl123@gmail.com>
2022-10-24 11:11:40 +0200

    neighbors function

--- main.py
+++ main.py
@@ -9,6 +9,32 @@ pygame.display.set_caption("MineSweeper")
 BG_COLOR = "white"
 ROWS, COLS = 30,30
 MINES = 15
+
+def get_neighbors(row, col, rows, cols):
+    neighbors=[]
+
+    if row > 0:                         #up
+        neighbors.append((row -1, col))
+    if row < len(rows) -1:              #down
+        neighbors.append((row +1, col))
+    if col > 0:                         #left
+        neighbors.append((row, col -1))
+    if col < len(cols) -1:              #right
+        neighbors.append((row, col +1))
+
+    if row > 0 and col > 0:                         #top-left
+        neighbors.append((row -1, col -1))
+    if row < len(rows) -1 and col < len(cols) -1:   #top-right 
+        neighbors.append((row +1, col +1))
+    if row < len(rows) -1 and col > 0:              #bot-left
+        neighbors.append((row +1, col -1))
+    if row > 0 and col < len(cols) -1:              #bot-right
+        neighbors.append((row -1, col-1))
+
+    return neighbors
+    
+
+
 def create_minefield(rows,cols, mines):
     field =[[0 for _ in range(cols)] for _ in range(rows)]
     mine_positions = set()
